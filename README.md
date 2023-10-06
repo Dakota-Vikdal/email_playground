ab7a4f70dab80f9b8019eac366035bad67b446bc
Dakota Vikdal <dakota.james19@hotmail.com>
2023-10-06 16:28:07 -0400

    finishing touches with comments and Readme file

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # email_playground
+In this project I've created a dummy gmail account that I used in order to send emails from Python. 
+
+I started with importing all the required libraries. 
+
+I then created an html file that holds a very basic html format with some text in the body. 
+
+Next I established various email details such as who the email is from, who it's going to and what the subject is. 
+
+The dynamic aspect of the html document was then utilized via a dictionary.
+
+Lastly, I used a with statement in order to grab the email I wished to send from.
+
+The rest is history. I walk away with a new tool to play with and implement in future projects. Thanks for reading.
--- email_sender.py
+++ email_sender.py
@@ -3,15 +3,20 @@ from email.message import EmailMessage
 from string import Template
 from pathlib import Path
 
-
+# Establish a connection between this file and the html file.
 html = Template(Path('index.html').read_text())
+
+# Create the basic email components such as who the email is from, who it is going to and
+# the subject of the email
 email = EmailMessage()
 email['from'] = 'Nat the Bat'
 email['to'] = 'yogadaddy666@gmail.com'
 email['subject'] = 'I am the coolest arent I!! THE COSMOS FLOW THROUGH ME LIKE A TURD FLOWS THROUGH YOU!'
 
+# Utilize the dynamic aspect of the html file. In this case, name.
 email.set_content(html.substitute({'name': '!NatAttack!'}), 'html')
 
+# Establish the email that will be sending this email
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
